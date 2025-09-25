3cfc5b3eabb38d2f686ac3f5de7d6b2be08b7d48
Vanessa <71130347+typewriterbot@users.noreply.github.com>
2025-09-24 20:32:03 -0400

    ADding chance of failure plus monitor implementati

--- Controller.java
+++ Controller.java
@@ -1,12 +1,18 @@
 public class Controller {
     
+    public static void Receiver(String status) { // detects failures, obstacles etc.
+        if (status.equals("Critical error"))
+            System.exit(-1); // or maybe a fix instead of exiting prgram?? 
+        System.out.println("Operating as normal...");
+    }
+
     public static void main(String [] args) { 
         Heartbeat heart = new Heartbeat();
         Monitor monitor = new Monitor(); 
-
         while (true) { 
-            monitor.checksHeart(heart);
+            Controller.Receiver(monitor.checksHeart(heart));            
         }
+
     }
 
 }
--- Heartbeat.java
+++ Heartbeat.java
@@ -1,18 +1,9 @@
 public class Heartbeat {
     String heart = "thump";
 
-    public void changeHeart(String word){
-        this.heart = word;
-    }
-
     public boolean isAlive() {
-        if (heart.equals("thump"))
-            return true;
-        return false; 
-    }
-
-    public static void main (String [] args) { 
-
+        System.out.println(heart);
+        return true; 
     }
 
 }
